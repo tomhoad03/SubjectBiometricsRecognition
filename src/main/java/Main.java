9302256a142dca3babed3aac31514c1cb2d91d99
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2023-03-24 14:26:57 +0000

    Removed testing and updated person localisation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,14 @@
-import gnu.trove.list.array.TFloatArrayList;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
-import org.openimaj.image.analysis.algorithm.EuclideanDistanceTransform;
 import org.openimaj.image.colour.ColourSpace;
-import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.connectedcomponent.GreyscaleConnectedComponentLabeler;
 import org.openimaj.image.pixel.ConnectedComponent;
 import org.openimaj.image.pixel.Pixel;
 import org.openimaj.image.pixel.PixelSet;
 import org.openimaj.image.processing.convolution.FFastGaussianConvolve;
-import org.openimaj.image.processing.edges.CannyEdgeDetector;
-import org.openimaj.image.processing.resize.ResizeProcessor;
-import org.openimaj.image.processing.threshold.OtsuThreshold;
 import org.openimaj.image.processor.PixelProcessor;
-import org.openimaj.image.typography.hershey.HersheyFont;
 import org.openimaj.ml.clustering.FloatCentroidsResult;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
 import org.openimaj.ml.clustering.kmeans.FloatKMeans;
@@ -30,16 +23,20 @@ public class Main {
         final VFSListDataset<MBFImage> training = new VFSListDataset<>(Paths.get("").toAbsolutePath() + "\\src\\main\\java\\biometrics\\training", ImageUtilities.MBFIMAGE_READER);
         final VFSListDataset<MBFImage> testing = new VFSListDataset<>(Paths.get("").toAbsolutePath() + "\\src\\main\\java\\biometrics\\testing", ImageUtilities.MBFIMAGE_READER);
 
-        ArrayList<TrainingImage> featureVectors = new ArrayList<>();
         int globalCount = 1;
 
         for (MBFImage trainingImage : training) {
             System.out.println("Training... " + globalCount);
 
+            // Resize and crop the image
             trainingImage = trainingImage.extractCenter(trainingImage.getWidth() / 2, (trainingImage.getHeight() / 2) + 120, 720, 1260);
+            MBFImage clonedTrainingImage = trainingImage.clone();
+
+            // Apply a Guassian blur to reduce noise
             trainingImage = ColourSpace.convert(trainingImage, ColourSpace.CIE_Lab);
             trainingImage.processInplace(new FFastGaussianConvolve(2, 2));
 
+            // Get the pixel data
             float[][] imageData = trainingImage.getPixelVectorNative(new float[trainingImage.getWidth() * trainingImage.getHeight()][3]);
 
             // Groups the pixels into their classes
@@ -47,7 +44,7 @@ public class Main {
             FloatCentroidsResult result = cluster.cluster(imageData);
             float[][] centroids = result.centroids;
 
-            // Assigns pixels to classes
+            // Assigns pixels to a class
             trainingImage.processInplace((PixelProcessor<Float[]>) pixel -> {
                 HardAssigner<float[], float[], IntFloatPair> assigner = result.defaultHardAssigner();
 
@@ -63,137 +60,36 @@ public class Main {
                 }
                 return set2;
             });
-
             trainingImage = ColourSpace.convert(trainingImage, ColourSpace.RGB);
 
-            // Get the connected components
+            // Get the two connected components
             GreyscaleConnectedComponentLabeler labeler = new GreyscaleConnectedComponentLabeler();
             List<ConnectedComponent> components = labeler.findComponents(trainingImage.flatten());
             ArrayList<Float> componentFeatureVectors = new ArrayList<>();
 
+            // Get the person component
             components.sort(Comparator.comparingInt(PixelSet::calculateArea));
             Collections.reverse(components);
+            ConnectedComponent personComponent = components.get(1);
 
-            trainingImage.processInplace(new CannyEdgeDetector());
+            // Get the boundary pixels and all contained pixels
+            List<Pixel> boundary = personComponent.getOuterBoundary();
+            Set<Pixel> pixels = personComponent.getPixels();
 
-            List<Pixel> boundary = components.get(1).getOuterBoundary();
-            trainingImage.fill(RGBColour.BLACK);
-
-            for (Pixel pixel : boundary) {
-                int x = (int) pixel.getX(), y = (int) pixel.getY();
-
-                trainingImage.getBand(0).pixels[y - 1][x - 1] = 1;
-                trainingImage.getBand(1).pixels[y - 1][x - 1] = 1;
-                trainingImage.getBand(2).pixels[y - 1][x - 1] = 1;
-            }
-
-            // Append all component distance vectors
-            for (ConnectedComponent component : components) {
-                if (component.calculateArea() > 50) {
-                    TFloatArrayList boundaryDistances = component.calculateBoundaryDistanceFromCentre(); // TODO This size of array is to random and so vectors don't line up
-
-                    float[] sortedBoundaryDistances = boundaryDistances.toArray();
-                    float[] normalisedBoundaryDistances = boundaryDistances.toArray();
-                    Arrays.sort(sortedBoundaryDistances);
-
-                    for (float normalisedBoundaryDistance : normalisedBoundaryDistances) {
-                        componentFeatureVectors.add(normalisedBoundaryDistance / sortedBoundaryDistances[sortedBoundaryDistances.length - 1]);
+            // Remove all unnecessary pixels from image
+            for (int y = 0; y < clonedTrainingImage.getHeight(); y++) {
+                for (int x = 0; x < clonedTrainingImage.getWidth(); x++) {
+                    if (!pixels.contains(new Pixel(x, y))) {
+                        clonedTrainingImage.getBand(0).pixels[y][x] = 1;
+                        clonedTrainingImage.getBand(1).pixels[y][x] = 1;
+                        clonedTrainingImage.getBand(2).pixels[y][x] = 1;
                     }
                 }
             }
 
+            // Example display of image
             globalCount++;
-            DisplayUtilities.display(trainingImage);
-        }
-
-        globalCount = 1;
-
-        for (MBFImage testingImage : testing) {
-            System.out.println("Testing... " + globalCount);
-
-            testingImage = ColourSpace.convert(testingImage, ColourSpace.CIE_Lab);
-            float[][] imageData = testingImage.getPixelVectorNative(new float[testingImage.getWidth() * testingImage.getHeight()][3]);
-
-            // Groups the pixels into their classes
-            FloatKMeans cluster = FloatKMeans.createExact(2);
-            FloatCentroidsResult result = cluster.cluster(imageData);
-            float[][] centroids = result.centroids;
-
-            // Assigns pixels to classes
-            testingImage.processInplace((PixelProcessor<Float[]>) pixel -> {
-                HardAssigner<float[], float[], IntFloatPair> assigner = result.defaultHardAssigner();
-
-                float[] set1 = new float[3];
-                for (int i = 0; i < 3; i++){
-                    set1[i] = pixel[i];
-                }
-                float[] centroid = centroids[assigner.assign(set1)];
-
-                Float[] set2 = new Float[3];
-                for (int i = 0; i < 3; i++){
-                    set2[i] = centroid[i];
-                }
-                return set2;
-            });
-
-            // Get the connected components
-            GreyscaleConnectedComponentLabeler labeler = new GreyscaleConnectedComponentLabeler();
-            List<ConnectedComponent> components = labeler.findComponents(testingImage.flatten());
-            ArrayList<Float> componentFeatureVectors = new ArrayList<>();
-
-            // Append all component distance vectors
-            for (ConnectedComponent component : components) {
-                if (component.calculateArea() > 10000) {
-                    TFloatArrayList boundaryDistances = component.calculateBoundaryDistanceFromCentre(); // TODO This size of array is to random and so vectors don't line up
-
-                    float[] sortedBoundaryDistances = boundaryDistances.toArray();
-                    float[] normalisedBoundaryDistances = boundaryDistances.toArray();
-                    Arrays.sort(sortedBoundaryDistances);
-
-                    for (float normalisedBoundaryDistance : normalisedBoundaryDistances) {
-                        componentFeatureVectors.add(normalisedBoundaryDistance / sortedBoundaryDistances[sortedBoundaryDistances.length - 1]);
-                    }
-                }
-            }
-
-            float distance = -1;
-            int count = 0;
-            int closest = 0;
-
-            for (TrainingImage trainingImage : featureVectors) {
-                ArrayList<Float> trainingFeatureVectors = trainingImage.getFeatureVector();
-                double sum = 0f;
-
-                for (int i = 0; i < trainingFeatureVectors.size() + componentFeatureVectors.size(); i++) {
-                    float value1 = 0f, value2 = 0f;
-
-                    if (i < trainingFeatureVectors.size()) {
-                        value1 = trainingFeatureVectors.get(i);
-                    }
-                    if (i < componentFeatureVectors.size()) {
-                        value2 = componentFeatureVectors.get(i);
-                    }
-
-                    if (value1 > 0f || value2 > 0f) {
-                        sum += Math.exp(value1 - value2);
-                    } else {
-                        break;
-                    }
-                }
-                float sqrt = (float) Math.sqrt(sum);
-
-                if (distance == -1 || distance > sqrt) {
-                    distance = sqrt;
-                    closest = count;
-                }
-                count++;
-            }
-
-            System.out.println(globalCount + " - " + featureVectors.get(closest).getId() + " - " + distance);
-            globalCount++;
-
-            testingImage = ColourSpace.convert(testingImage, ColourSpace.RGB);
-            DisplayUtilities.display(testingImage);
+            DisplayUtilities.display(clonedTrainingImage);
         }
 
         System.out.println("Finished!");
